985e248cd39593eb697cb6e04d4d7ae850759ad5
Alao-Abiodun <abiodunDev@gmail.com>
2020-06-16 14:29:37 +0100

    fix: refrator the codebase

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ import { signUp, signIn, profile } from '../controllers/user.controller';
 router.post('/api/v1/auth/signup', signUp);
 router.post('/api/v1/auth/signin', signIn);
 //Handle get request for a single user profile details
-router.get('/api/v1/auth/profile/:user_id', profile);
+router.get('/api/v1/profile/:id', profile);
 
 export default router;
 
